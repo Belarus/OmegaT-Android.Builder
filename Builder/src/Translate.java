96c8f09da112ef73db5b9646da5b6eceb2a11a3a
Alex Buloichik <alex73mail@gmail.com>
2012-03-27 16:31:05 +0300

    Fix update.zip

--- Builder/src/Translate.java
+++ Builder/src/Translate.java
@@ -1,10 +1,14 @@
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.List;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
+import java.util.zip.ZipOutputStream;
 
 import org.alex73.android.StyledString;
 import org.alex73.android.arsc2.ResourceProcessor;
@@ -12,6 +16,8 @@ import org.alex73.android.arsc2.Translation;
 import org.alex73.android.arsc2.reader.ChunkReader2;
 import org.alex73.android.common.FileInfo;
 import org.alex73.android.common.zip.ApkUpdater;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 public class Translate {
     static TranslationDebug tr;
@@ -20,37 +26,90 @@ public class Translate {
         tr = new TranslationDebug(new GZIPInputStream(new FileInputStream(
                 "../Installer/res/raw/translation.bin")));
         for (String f : args) {
-            process(new File(f));
+            if (f.endsWith(".apk")) {
+                processApk(new File(f));
+            } else if (f.endsWith(".zip")) {
+                processZip(new File(f));
+            }
         }
     }
 
-    static void process(File f) throws Exception {
+    static void processZip(File f) throws Exception {
+        TranslateSignatures sig = new TranslateSignatures();
+        File temp = new File("temp.data");
+
+        ZipFile zip = new ZipFile(f);
+        ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream(f.getParent() + "/out-"
+                + f.getName()));
+        for (Enumeration<? extends ZipEntry> it = zip.entries(); it.hasMoreElements();) {
+            ZipEntry en = it.nextElement();
+            if (en.getName().equals("META-INF/CERT.SF") || en.getName().equals("META-INF/CERT.RSA")
+                    || en.getName().equals("META-INF/MANIFEST.MF")) {
+                continue;
+            }
+            InputStream in = zip.getInputStream(en);
+            byte[] data = IOUtils.toByteArray(in);
+            in.close();
+            if (en.getName().endsWith(".apk")) {
+                FileUtils.writeByteArrayToFile(temp, data);
+                File o = processApk(temp);
+                if (o != null) {
+                    data = FileUtils.readFileToByteArray(o);
+                }
+            } else if (en.getName().equals("system/build.prop")) {
+                FileUtils.writeByteArrayToFile(temp, data);
+                processProp(temp);
+                data = FileUtils.readFileToByteArray(temp);
+            }
+            zipOut.putNextEntry(new ZipEntry(en.getName()));
+            IOUtils.write(data, zipOut);
+
+            sig.putDigestToManifest(en, data);
+        }
+
+        sig.writeTo(zipOut);
+        zip.close();
+        zipOut.close();
+    }
+
+    static File processApk(File f) throws Exception {
         ResourceProcessor rs;
         FileInfo fi = new FileInfo(f);
         fi.readManifestInfo();
-        ZipFile zip = new ZipFile(fi.localFile);
-        try {
+        if (tr.isPackageTranslated(fi.packageName)) {
+            ZipFile zip = new ZipFile(fi.localFile);
             ZipEntry en = zip.getEntry("resources.arsc");
 
             InputStream in = zip.getInputStream(en);
-            try {
-                ChunkReader2 rsReader = new ChunkReader2(in);
-                rs = new ResourceProcessor(rsReader);
-
-            } finally {
-                in.close();
-            }
-        } finally {
+            ChunkReader2 rsReader = new ChunkReader2(in);
+            rs = new ResourceProcessor(rsReader);
+            in.close();
             zip.close();
+
+            rs.process(fi.packageName, tr);
+
+            byte[] translatedResources = rs.save();
+
+            File fo = new File(fi.localFile.getAbsolutePath() + ".out");
+            fo.delete();
+            new ApkUpdater().replace(fi.localFile, fo, translatedResources);
+            return fo;
+        } else {
+            return null;
         }
+    }
 
-        rs.process(fi.packageName, tr);
-
-        byte[] translatedResources = rs.save();
-
-        File fo = new File(fi.localFile.getParent() + "/out/", fi.localFile.getName());
-        fo.getParentFile().mkdirs();
-        new ApkUpdater().replace(fi.localFile, fo, translatedResources);
+    static void processProp(File f) throws Exception {
+        List<String> lines = FileUtils.readLines(f, "UTF-8");
+        for (int i = 0; i < lines.size(); i++) {
+            if (lines.get(i).startsWith("ro.product.locale.language=")) {
+                lines.set(i, "ro.product.locale.language=be");
+            }
+            if (lines.get(i).startsWith("ro.product.locale.region=")) {
+                lines.set(i, "ro.product.locale.region=BY");
+            }
+        }
+        FileUtils.writeLines(f, "UTF-8", lines, "\n");
     }
 
     static class TranslationDebug extends Translation {
