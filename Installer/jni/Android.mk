c2fa655097a38a2ea7a169307a4a5927a1185115
Alex Buloichik <alex73mail@gmail.com>
2012-11-21 17:39:22 +0300

    Native mv

--- Installer/jni/Android.mk
+++ Installer/jni/Android.mk
@@ -6,6 +6,15 @@ LOCAL_MODULE    := stat
 LOCAL_SRC_FILES := stat.c
 
 ## logging
-LOCAL_LDLIBS := -llog
+## LOCAL_LDLIBS := -llog
 
 include $(BUILD_SHARED_LIBRARY)
+
+
+# ========================================================
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := mv
+LOCAL_SRC_FILES := mv.c
+
+include $(BUILD_EXECUTABLE)
