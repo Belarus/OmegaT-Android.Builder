1324cb100c947c100fa52e742adbc18e3c2994fc
Alex Buloichik <alex73mail@gmail.com>
2012-11-23 09:51:00 +0300

    Fix replace files

--- Installer/build_jni.sh
+++ Installer/build_jni.sh
@@ -4,3 +4,9 @@ set -x
 javah -o jni/stat.h -classpath bin/classes org.alex73.android.common.JniWrapper
 /data/opt/android-ndk-r8c/ndk-build clean
 /data/opt/android-ndk-r8c/ndk-build NDK_DEBUG=0
+
+for i in libs/*; do
+  archfrom=`basename $i`
+  archto=`basename $i | tr - _`
+  mv libs/$archfrom/replacer res/raw/replacer_$archto.bin
+done
